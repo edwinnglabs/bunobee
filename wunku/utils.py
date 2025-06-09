5383f84b9428d17c9eaab0dc759402b4a5b28389
edwinnglabs <edwinnglabs@gmail.com>
2025-06-09 10:25:46 -0700

    finish a prototype for wbic optimization

--- wunku/utils.py
+++ wunku/utils.py
@@ -2,6 +2,7 @@ import time
 import arviz as az
 import xarray as xr
 import pandas as pd
+import jax.numpy as jnp
 
 def generate_seed():
     return int(time.time())
@@ -12,3 +13,7 @@ def summarize_posteriors(posteriors: xr.Dataset) -> pd.DataFrame:
     # idata = az.from_xarray(posterior=posteriors)
     summary_df = az.summary(posteriors)
     return summary_df
+
+def flatten_front_dim(x: jnp.array, n: int) -> jnp.array:
+    new_x = x.reshape(-1, *x.shape[n:])
+    return new_x
